ec29ac0be6e5fe2538d5fc763147e5f552319aa6
Thanatcha Romlamduan <thanatcha8257@gmail.com>
2026-09-03 00:23:30 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,18 +1,9 @@
 <?php
-// logout.php
-// สำหรับออกจากระบบและทำลาย Session
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
-// ลบตัวแปร Session ทั้งหมด
 $_SESSION = [];
-
-// ทำลาย Session
 session_destroy();
-
-// นำกลับไปยังหน้าเข้าสู่ระบบ
 header('Location: login.php');
 exit();
 ?>
